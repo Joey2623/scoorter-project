b1efc7fc16fe77126945d5a42fcb2982296ac3ea
Joey2623 <joanne.greenwood2@barclays.comgit config --global user.name Joey2623git config --global user.email joanne.greenwood2@barclays.com>
2024-05-10 16:19:16 +0100

    part complete

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -1,7 +1,36 @@
-// require the User and Scooter classes - see where they can be used in ScooterApp.js
+const Scooter = require("./Scooter")
+const User = require("./User")
 
 class ScooterApp {
-  // ScooterApp code here
+constructor () {
+  this.station = {
+    'Seaforth' : [],
+    'Lime Street' : [],
+    'Formby' : []
+  }
+  this.registeredUsers = {}
+}
+
+registerUser(username, password, age) {
+  if (this.registeredUsers[username]) {
+    throw new Error('Already registered')
+  } if (age < 18) {
+    throw new Error('Too young to register')
+  }
+  const user = new User(username, password, age);
+  this.registeredUsers[username] = user;
+  console.log(`${username} has been registered.`);
+  return user;
+}
+
+loginUser(username, password) {
+  User.login(password)
+  if (true)
+    console.log(`${username} has been logged in.`);
+  if (!User || User.password !== password) {
+    throw new Error('Username or password is incorrect.');
+  }
+}
 }
 
 module.exports = ScooterApp
